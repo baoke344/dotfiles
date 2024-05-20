e92fc584fad7d955524630a1572ef44a0642ea3a
baoke344 <baoanh301094@gmail.com>
2024-05-20 21:45:37 +0700

    add awesome config

--- qtile/autostart_once.sh
+++ qtile/autostart_once.sh
@@ -12,3 +12,9 @@ nm-applet &
 #start fcitx5
 #fcitx5 &
 copyq &
+
+#run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+ #run "picom" -b
+ #redshift -P -O 5000
+ #run "greenclip" daemon
+ ## run "xsettingsd"
